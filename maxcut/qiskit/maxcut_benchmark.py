1f71e709836f95465f4a9e23b5322be39e58f9ea
rtvuser1 <tlubinski1@gmail.com>
2022-01-05 21:50:26 -0800

    First pass on implementing MaxCut benchmark, method 1 (note: result analysis not yet implemented)

--- maxcut/qiskit/maxcut_benchmark.py
+++ maxcut/qiskit/maxcut_benchmark.py
@@ -4,6 +4,7 @@ MaxCut Benchmark Program - Qiskit
 
 import sys
 import time
+from collections import namedtuple
 
 import numpy as np
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
@@ -12,6 +13,8 @@ sys.path[1:1] = [ "_common", "_common/qiskit" ]
 sys.path[1:1] = [ "../../_common", "../../_common/qiskit" ]
 import execute as ex
 import metrics as metrics
+
+import common       # from lanl-ansi-max-cut
 
 np.random.seed(0)
 
@@ -24,189 +27,66 @@ num_resets = 1
 QC_ = None
 Uf_ = None
 
+# based on examples from https://qiskit.org/textbook/ch-applications/qaoa.html
+QAOA_Parameter  = namedtuple('QAOA_Parameter', ['beta', 'gamma'])
+
+
 ############### Circuit Definition
-'''
-def create_oracle(num_qubits, input_size, secret_int):
-    # Initialize first n qubits and single ancilla qubit
-    qr = QuantumRegister(num_qubits)
-    qc = QuantumCircuit(qr, name=f"Uf")
+  
+# Create ansatz specific to this problem, defined by G = nodes, edges, and the given parameters
+def create_qaoa_circ(nqubits, edges, parameters):
 
-    # perform CX for each qubit that matches a bit in secret string
-    s = ('{0:0' + str(input_size) + 'b}').format(secret_int)
-    for i_qubit in range(input_size):
-        if s[input_size - 1 - i_qubit] == '1':
-            qc.cx(qr[i_qubit], qr[input_size])
+    qc = QuantumCircuit(nqubits)
+
+    # initial_state
+    for i in range(0, nqubits):
+        qc.h(i)
+
+    for par in parameters:
+        # problem unitary
+        for i,j in edges:
+            qc.rzz(2 * par.gamma, i, j)
+
+        qc.barrier()
+        
+        # mixer unitary
+        for i in range(0, nqubits):
+            qc.rx(2 * par.beta, i)
+
+    qc.measure_all()
+
     return qc
-'''   
-
-   
-def qft_dagger(qc, clock, n):      
-    qc.h(clock[1]);
-    for j in reversed(range(n)):
-      for k in reversed(range(j+1,n)):
-        qc.cu1(-np.pi/float(2**(k-j)), clock[k], clock[j]);
-    qc.h(clock[0]);
-    qc.swap(clock[0], clock[1]);
-
-def qft(qc, clock, n):
-    qc.swap(clock[0], clock[1]);
-    qc.h(clock[0]);
-    for j in reversed(range(n)):
-      for k in reversed(range(j+1,n)):
-        qc.cu1(np.pi/float(2**(k-j)), clock[k], clock[j]);
-    qc.h(clock[1]);
- 
-def qpe(qc, clock, target):
-    qc.barrier()
-
-    # e^{i*A*t}
-    qc.cu(np.pi/2, -np.pi/2, np.pi/2, 3*np.pi/4, clock[0], target, label='U');
     
-    # e^{i*A*t*2}
-    qc.cu(np.pi, np.pi, 0, 0, clock[1], target, label='U2');
-
-    qc.barrier();
+# Create the benchmark program 
+def MaxCut (num_qubits, secret_int, edges, method = 1):
     
-    # Perform an inverse QFT on the register holding the eigenvalues
-    qft_dagger(qc, clock, 2)
-    
-def inv_qpe(qc, clock, target):
-    
-    # Perform a QFT on the register holding the eigenvalues
-    qft(qc, clock, 2)
-
-    qc.barrier()
-
-    # e^{i*A*t*2}
-    qc.cu(np.pi, np.pi, 0, 0, clock[1], target, label='U2');
-
-    # e^{i*A*t}
-    qc.cu(np.pi/2, np.pi/2, -np.pi/2, -3*np.pi/4, clock[0], target, label='U');
-
-    qc.barrier()
-
-def maxcut_routine(qc, ancilla, clock, input, measurement):
-    
-    qpe(qc, clock, input)
-
-    qc.barrier()
-    
-    # This section is to test and implement C = 1
-    qc.cry(np.pi, clock[0], ancilla)
-    qc.cry(np.pi/3, clock[1], ancilla)
-
-    qc.barrier()
-    
-    qc.measure(ancilla, measurement[0])
-    qc.barrier()
-    inv_qpe(qc, clock, input)
-    
-
-
-def MaxCut (num_qubits, secret_int, beta, method = 1):
-    
-    # Create the various registers needed
-    clock = QuantumRegister(2, name='clock')
-    input = QuantumRegister(1, name='b')
-    ancilla = QuantumRegister(1, name='ancilla')
-    measurement = ClassicalRegister(2, name='c')
-
-    # Create an empty circuit with the specified registers
-    qc = QuantumCircuit(ancilla, clock, input, measurement)
-
-    # size of input is one less than available qubits
-    input_size = num_qubits - 1
-
+    # Method 1 - execute one instance of ansatz and determine fidelity
     if method == 1:
-        # allocate qubits
-        '''
-        qr = QuantumRegister(num_qubits); cr = ClassicalRegister(input_size); qc = QuantumCircuit(qr, cr, name="main")
-
-        # put ancilla in |1> state
-        qc.x(qr[input_size])
-
-        # start with Hadamard on all qubits, including ancilla
-        for i_qubit in range(num_qubits):
-             qc.h(qr[i_qubit])
-
-        qc.barrier()
-
-        #generate Uf oracle
-        Uf = create_oracle(num_qubits, input_size, secret_int)
-        qc.append(Uf,qr)
-
-        qc.barrier()
-
-        # start with Hadamard on all qubits, including ancilla
-        for i_qubit in range(num_qubits):
-             qc.h(qr[i_qubit])
-
-        # uncompute ancilla qubit, not necessary for algorithm
-        qc.x(qr[input_size])
-
-        qc.barrier()
-
-        # measure all data qubits
-        for i in range(input_size):
-            qc.measure(i, i)
-
-        global Uf_
-        if Uf_ == None or num_qubits <= 6:
-            if num_qubits < 9: Uf_ = Uf
-        '''
-        qc.barrier()
+    
+        # create ansatz circuit
         
-        # State preparation. (various initial values)
-        # intial_state = [0,1]
-        # intial_state = [1,0]
-        # intial_state = [1/np.sqrt(2),1/np.sqrt(2)]
-        # intial_state = [np.sqrt(0.9),np.sqrt(0.1)]
+        ROUNDS = 2
+        theta = 2*ROUNDS*[0.7]
         
-        ##intial_state = [np.sqrt(1 - beta), np.sqrt(beta)]
-        ##qc.initialize(intial_state, 3)
+        # put parameters into the form expected by the ansatz generator
+        p = len(theta)//2  # number of qaoa rounds
+        beta = theta[:p]
+        gamma = theta[p:]
+        parameters = [QAOA_Parameter(*t) for t in zip(beta,gamma)]
         
-        # use an RY rotation to initialize the input state between 0 and 1
-        qc.ry(2 * np.arcsin(np.sqrt(beta)), input)
+        qc = create_qaoa_circ(num_qubits, edges, parameters)   
 
-        ##qc.barrier()
-
-        # Put clock qubits into uniform superposition
-        qc.h(clock)
-
-        hhl_routine(qc, ancilla, clock, input, measurement)
-
-        # Perform a Hadamard Transform
-        qc.h(clock)
-
-        qc.barrier()
-
-        qc.measure(input, measurement[1])
-
-
+    # Method 2 - execute multiple instances of ansatz, primarily to measure execution time average
+    # DEVNOTE: unclear if this is the desired approach yet
     elif method == 2:
-        # allocate qubits
-        qr = QuantumRegister(2); cr = ClassicalRegister(input_size);
-        qc = QuantumCircuit(qr, cr, name="main")
-
-        # put ancilla in |-> state
-        qc.x(qr[1])
-        qc.h(qr[1])
-
-        qc.barrier()
-
-        # perform CX for each qubit that matches a bit in secret string
-        s = ('{0:0' + str(input_size) + 'b}').format(secret_int)
-        for i in range(input_size):
-            if s[input_size - 1 - i] == '1':
-                qc.h(qr[0])
-                qc.cx(qr[0], qr[1])
-                qc.h(qr[0])
-            qc.measure(qr[0], cr[i])
-
-            # Perform num_resets reset operations
-            qc.reset([0]*num_resets)
-
-    # save smaller circuit example for display
+        pass
+    
+    # Method 3 - include classical minimizer to control execution and to obtain optimal result
+    # Need to build in some sophistication here to control for time/quality profile determination - TBD
+    elif method == 3:
+        pass
+        
+    # save small circuit example for display
     global QC_
     if QC_ == None or num_qubits <= 6:
         if num_qubits < 9: QC_ = qc
@@ -216,6 +96,10 @@ def MaxCut (num_qubits, secret_int, beta, method = 1):
 
  
 ############### Result Data Analysis
+
+# DEVNOTE:
+# The section below on Result Data Analysis needs work, it is just pulled from HHL
+# Need to implement this by pre-calculating the expectation values using noiseless simulator
 
 saved_result = None
 
@@ -280,6 +164,7 @@ def analyze_and_print_result (qc, result, num_qubits, secret_int, num_shots):
     
     return counts, fidelity
 
+
 ################ Benchmark Loop
 
 # Execute program with default parameters
@@ -292,13 +177,8 @@ def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=100,
     # validate parameters (smallest circuit is 4 qubits)
     max_qubits = max(4, max_qubits)
     min_qubits = min(max(4, min_qubits), max_qubits)
+    max_circuits = min(10, max_circuits)
     #print(f"min, max qubits = {min_qubits} {max_qubits}")
-
-    # Variable for new qubit group ordering if using mid_circuit measurements
-    mid_circuit_qubit_group = []
-
-    # If using mid_circuit measurements, set transform qubit group to true
-    transform_qubit_group = True if method ==2 else False
     
     # Initialize metrics module
     metrics.init_metrics()
@@ -321,39 +201,41 @@ def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=100,
 
     # Execute Benchmark Program N times for multiple circuit sizes
     # Accumulate metrics asynchronously as circuits complete
-    for num_qubits in range(min_qubits, max_qubits + 1):
+    # DEVNOTE: increment by 2 to match the collection of problems in 'instance' folder
+    for num_qubits in range(min_qubits, max_qubits + 1, 2):
             
         # determine number of circuits to execute for this group
-        num_circuits = min(2**(num_qubits), max_circuits)
+        #num_circuits = min(2**(num_qubits), max_circuits)
+        num_circuits = max_circuits
         
         print(f"************\nExecuting [{num_circuits}] circuits with num_qubits = {num_qubits}")
-        
-        ''' 
-        #beta range not calculated dynamically yet
-        num_counting_qubits = 1
-        
-        # determine range of secret strings to loop over
-        if 2**(num_counting_qubits) <= max_circuits:
-            beta_range = [i/(2**(num_counting_qubits)) for i in list(range(num_circuits))]
-        else:
-            beta_range = [i/(2**(num_counting_qubits)) for i in np.random.choice(2**(num_counting_qubits), num_circuits, False)]
-        '''
-        
-        # supply hard-coded beta array (during initial testing)
-        beta_range = [ 1.0, 0.0, 0.9, 0.1, 0.8, 0.2 ]
-        if max_circuits < len(beta_range): beta_range = beta_range[:max_circuits]   
-        
+          
         # loop over limited # of inputs for this
-        for i in range(len(beta_range)):
-            beta = beta_range[i]
+        # assume the solution files start with 3 and go up from there
+        circuits_done = 0
+        for i in range(3, 3 + max_circuits + 1):
             
-            # create integer that represents beta to precision 4; use s_int as circuit id
-            s_int = int(beta * 10000)
-            print(f"  ... i={i} s_int={s_int}  beta={beta}")
+            # create integer that represents the problem instance; use s_int as circuit id
+            s_int = i
+            print(f"  ... i={i} s_int={s_int}")
+            
+            # create filename from num_qubits and circuit_id (s_int), then load the problem file
+            instance_filename = f"instance/mc_{str(num_qubits).zfill(3)}_{str(i).zfill(3)}_000.txt"
+            #print(f"... instance_filename = {instance_filename}")
+            nodes, edges = common.read_maxcut_instance(instance_filename)
+            #print(f"nodes = {nodes}")
+            #print(f"edges = {edges}")
+            
+            # if the file does not exist, we are done with this number of qubits
+            if nodes == None:
+                print(f"  ... problem {str(i).zfill(3)} not found, limiting to {circuits_done} circuit(s).")
+                break;
+            
+            circuits_done += 1
             
             # create the circuit for given qubit size and secret string, store time metric
             ts = time.time()
-            qc = MaxCut(num_qubits, s_int, beta, method)
+            qc = MaxCut(num_qubits, s_int, edges, method)
             metrics.store_metric(num_qubits, s_int, 'create_time', time.time()-ts)
 
             # collapse the sub-circuit levels used in this benchmark (for qiskit)
@@ -373,8 +255,7 @@ def run (min_qubits=3, max_qubits=6, max_circuits=3, num_shots=100,
     #if method == 1: print("\nQuantum Oracle 'Uf' ="); print(Uf_ if Uf_ != None else " ... too large!")
 
     # Plot metrics for all circuit sizes
-    metrics.plot_metrics(f"Benchmark Results - MaxCut ({method}) - Qiskit",
-                         transform_qubit_group = transform_qubit_group, new_qubit_group = mid_circuit_qubit_group)
+    metrics.plot_metrics(f"Benchmark Results - MaxCut ({method}) - Qiskit")
 
 # if main, execute method
 if __name__ == '__main__': run()
