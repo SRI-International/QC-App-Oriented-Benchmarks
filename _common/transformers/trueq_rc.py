53c40ed905de6544965533501aa08e4684a7b031
hammamqassim <hammam.qassim@keysight.com>
2022-01-16 21:10:52 -0500

    A bugfix and other changes to the compilers in trueq_rc.py

--- _common/transformers/trueq_rc.py
+++ _common/transformers/trueq_rc.py
@@ -7,25 +7,53 @@ import numpy as np
 import trueq.compilation as tqc
 import qiskit as qs
 
-def do_rc(circuit, backend, n_compilations=20):
-    print("  ... performing randomized compilation.")
-    rc_compressed_compiler = tq.Compiler.basic(
+def local_rc(circuit, backend, n_compilations=20):   
+    compiler = tq.Compiler.basic(
+    entangler=tq.Gate.cnot, # Must choose a Clifford entangler for RC
+    passes=(tqc.Native2Q,
+            tqc.UnmarkCycles,
+            tqc.MarkCycles,
+            tqc.RCLocal,
+            tqc.UnmarkCycles,
+            tqc.Justify
+        )
+    )
+
+    tq.interface.qiskit.set_from_backend(backend)
+    circuit_tq, metadata = tq.interface.qiskit.to_trueq_circ(circuit)
+    
+    for qubit in circuit.qubits:
+        if qubit.index not in circuit_tq.labels:
+            del metadata.mapping[qubit.index]
+
+    rc_collection = [
+                tq.interface.qiskit.from_trueq_circ(compiler.compile(circuit_tq), 
+                metadata=metadata)
+                for j in range(n_compilations)
+    ]
+    
+    return rc_collection
+        
+def full_rc(circuit, backend, n_compilations=20):
+    compiler = tq.Compiler.basic(
         entangler=tq.Gate.cnot, # Must choose a Clifford entangler for RC
         passes=(tqc.Native2Q,
-                tqc.MarkCycles,
-                tqc.RCLocal,
                 tqc.UnmarkCycles,
-                tqc.Merge,
-                tqc.RemoveId, 
-                tqc.RemoveEmptyCycle,
-                tqc.MarkCycles
+                tqc.MarkCycles,
+                tqc.RCCycle,
+                tqc.Justify
             )
     )
-    qiskit_passes = None
     tq.interface.qiskit.set_from_backend(backend)
-    circuit_tq =tqc.UnmarkCycles().apply(circuit.to_trueq()) 
-    rc_collection = [rc_compressed_compiler.compile(circuit_tq).to_qiskit(passes=qiskit_passes)
-            for j in range(n_compilations)
-                    ]
-    return rc_collection
+    circuit_tq, metadata = tq.interface.qiskit.to_trueq_circ(circuit)
     
+    for qubit in circuit.qubits:
+        if qubit.index not in circuit_tq.labels:
+            del metadata.mapping[qubit.index]
+    
+    rc_collection = [
+                tq.interface.qiskit.from_trueq_circ(compiler.compile(circuit_tq), 
+                metadata=metadata)
+                for j in range(n_compilations)
+    ]
+    return rc_collection
