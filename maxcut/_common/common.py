48d020d0edb1ac0d58ae2d9bf69de3430c9672e1
rtvuser1 <tlubinski1@gmail.com>
2022-08-25 16:02:11 -0700

    Restored INSTANCE_DIR to maxcut/_common file; fix def of degree = -3

--- maxcut/_common/common.py
+++ maxcut/_common/common.py
@@ -1,5 +1,7 @@
 import os
 
+INSTANCE_DIR = 'instances'
+
 def read_maxcut_instance(file_path, _instances=None):
     if isinstance(_instances, dict):
         inst = os.path.splitext(os.path.basename(file_path))[0]
