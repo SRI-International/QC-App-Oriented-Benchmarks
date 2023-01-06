7e0c229e4b16f181bb1c728d5a291715588a5f77
rtvuser1 <tlubinski1@gmail.com>
2023-01-05 22:27:58 -0800

    For Max-Cur area plots, change label to Problem Size and improve autoscaling of X axis

--- _common/metrics.py
+++ _common/metrics.py
@@ -1523,7 +1523,7 @@ known_x_labels = {
     'cumulative_create_time' : 'Cumulative Circuit Creation Time (s)',
     'cumulative_elapsed_time' : 'Cumulative Elapsed Quantum Execution Time (s)',
     'cumulative_exec_time' : 'Cumulative Quantum Execution Time (s)',
-    'cumulative_opt_exec_time' : 'Cumulative Classical Optimizer Time (s)',
+    'cumulative_opt_exec_time' : 'Cumulative Classical Optimization Time (s)',
     'cumulative_depth' : 'Cumulative Circuit Depth'
 }
 
@@ -1538,7 +1538,9 @@ x_label_save_str = {
 
 # map known Y metrics to labels
 known_y_labels = {
-    'num_qubits' : 'Circuit Width'
+    #'num_qubits' : 'Circuit Width'     # this is only used for max-cut area plots 
+    #'num_qubits' : 'Problem Size (Number of Variables)'      # use Problem Size instead
+    'num_qubits' : 'Problem Size (# of Variables)'      # use Problem Size instead
 }
 # map known Score metrics to labels    
 known_score_labels = {
@@ -2172,14 +2174,13 @@ def get_distribution_and_stats():
             group_metrics_optgaps['cvar_ratio']['gapvals'].append(abs(1.0 - mets["cvar_ratio"]) * 100)
             group_metrics_optgaps['bestcut_ratio']['gapvals'].append(abs(1.0 - mets["bestcut_ratio"]) * 100)
             group_metrics_optgaps['gibbs_ratio']['gapvals'].append(abs(1.0 - mets["gibbs_ratio"]) * 100)
-
+            
             # Also store the optimality gaps at the three quantiles values
             # Here, optgaps are defined as weight(cut)/weight(maxcut) * 100
             q_vals = mets["quantile_optgaps"] # in fraction form. List of floats
             q_vals = [q_vals[i] * 100 for i in range(len(q_vals))] # In percentages
             group_metrics_optgaps['quantile_optgaps']['gapvals'].append(q_vals)
             
-
             # Store empirical distribution of cut size values / optimal value 
             unique_sizes = circuit_metrics_final_iter[group][str(circuit_id)]['unique_sizes']
             unique_counts = circuit_metrics_final_iter[group][str(circuit_id)]['unique_counts']
@@ -3011,12 +3012,17 @@ def plot_metrics_background(suptitle, ylabel, x_label, score_label,
 
     plt.title(suptitle)
     
-    # round the max up to be divisible evenly (in multiples of 0.1) by num_xdivs 
+    # DEVNOTE: this code could be made more general, rounding the axis max to 20 divs nicely
+    # round the max up to be divisible evenly (in multiples of 0.05 or 0.005) by num_xdivs 
     num_xdivs = 20
     max_base = num_xdivs * 0.05
-    x_max = max_base * int((x_max + max_base) / max_base)
+    if x_max != None and x_max > 1.0:
+        x_max = max_base * int((x_max + max_base) / max_base)
+    if x_max != None and x_max > 0.1:
+        max_base = num_xdivs * 0.005
+        x_max = max_base * int((x_max + max_base) / max_base)
     
-    #print(f"... {x_min} {x_max} {max_base} {x_max}")
+    #print(f"... {x_min} {x_max} {max_base}")
     if x_min < 0.1: x_min = 0
     
     # and compute the step size for the tick divisions
