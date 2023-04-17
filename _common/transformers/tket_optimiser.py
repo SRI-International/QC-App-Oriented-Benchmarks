af5d74be61fe5583e64c223804b53466eee9d8a5
dan-mills-cqc <danielmills@Daniels-MacBook-Pro.local>
2023-04-17 13:50:20 +0100

    Add transformer generation method

--- _common/transformers/tket_optimiser.py
+++ _common/transformers/tket_optimiser.py
@@ -24,112 +24,69 @@ from qiskit.circuit.quantumcircuit import QuantumCircuit
 def rebase_pass():
     return auto_rebase_pass({OpType.CX, OpType.X, OpType.SX, OpType.Rz})
 
-def gen_pass_list(backend, quick=False, approximate=False):
+def tket_transformer_generator(quick=False, cx_fidelity=1.0):
+    """Generator for transformer using TKET passes
 
-    # Obtain device data for noise aware placement.
-    characterisation = process_characterisation(backend)
-    averaged_errors = get_avg_characterisation(characterisation)
+    :param quick: Perform quick optimisation, defaults to False
+    :type quick: bool, optional
+    :param cx_fidelity: Estimated CX gate fidelity, defaults to 1.0
+    :type cx_fidelity: float, optional
+    """
 
-    # Initialise pass list and perform thorough optimisation.
-    pass_list = [DecomposeBoxes()]
-    if not quick: pass_list.append(FullPeepholeOptimise())
+    def transformation_method(circuit:QuantumCircuit, backend) -> list[QuantumCircuit]:
+        """Transformer using TKET optimisation passes.
 
-    # Add noise aware placement and routing to pass list.
-    coupling_map = backend.configuration().coupling_map
-    if coupling_map:
-        arch = Architecture(coupling_map)
-        pass_list.append(
-            CXMappingPass(
-                arch,
-                NoiseAwarePlacement(
+        :param circuit: Circuit to be optimised
+        :type circuit: QuantumCircuit
+        :param backend: Backed on which circuit is run.
+        :return: List of transformed circuits
+        :rtype: list[QuantumCircuit]
+        """
+
+        # Obtain device data for noise aware placement.
+        characterisation = process_characterisation(backend)
+        averaged_errors = get_avg_characterisation(characterisation)
+
+        # Initialise pass list and perform thorough optimisation.
+        pass_list = [DecomposeBoxes()]
+        if not quick: pass_list.append(FullPeepholeOptimise())
+
+        # Add noise aware placement and routing to pass list.
+        coupling_map = backend.configuration().coupling_map
+        if coupling_map:
+            arch = Architecture(coupling_map)
+            pass_list.append(
+                CXMappingPass(
                     arch,
-                    averaged_errors["node_errors"],
-                    averaged_errors["edge_errors"],
-                    averaged_errors["readout_errors"],
-                ),
-                directed_cx=False,
+                    NoiseAwarePlacement(
+                        arch,
+                        averaged_errors["node_errors"],
+                        averaged_errors["edge_errors"],
+                        averaged_errors["readout_errors"],
+                    ),
+                    directed_cx=False,
+                )
             )
-        )
 
-    if (not quick) and approximate: pass_list.append(KAKDecomposition(cx_fidelity=0.9))
-    if not quick: pass_list.extend([CliffordSimp(False), SynthesiseTket()])
+        if not quick:
+            pass_list.append(KAKDecomposition(cx_fidelity=0.9))
+            pass_list.extend([CliffordSimp(False), SynthesiseTket()])
 
-    # Rebase to backend gate set and perform basic optimisation
-    pass_list.append(rebase_pass())
+        # Rebase to backend gate set and perform basic optimisation
+        pass_list.append(rebase_pass())
 
-    if not quick:
-        pass_list.extend(
-            [
-                RemoveRedundancies(),
-                SimplifyInitial(allow_classical=False, create_all_qubits=True),
-            ]
-        )
+        if not quick:
+            pass_list.extend(
+                [
+                    RemoveRedundancies(),
+                    SimplifyInitial(allow_classical=False, create_all_qubits=True),
+                ]
+            )
 
-    return pass_list
+        tk_circuit = qiskit_to_tk(circuit)
+        SequencePass(pass_list).apply(tk_circuit)
+        circuit = tk_to_qiskit(tk_circuit)
 
+        return [circuit]
 
-def approximate_optimisation(circuit:QuantumCircuit, backend) -> list[QuantumCircuit]:
-    """Perform thourough but generic optimisation using 
-    TKET optimisation tool. Gates are approximately synthesised
-    to further reduce gate count.
-
-    :param circuit: Circuit to be optimised.
-    :type circuit: QuantumCircuit
-    :param backend: Backend which circuit should be optimised to.
-    :type backend: BaseBackend
-    :return: Optimised circuit.
-    :rtype: list[QuantumCircuit]
-    """
-    print("  ... performing high TKET approximate optimisation.")
-
-    pass_list = gen_pass_list(backend, quick=False, approximate=True)
-
-    # Optimise circuit using constructed pass list
-    tk_circuit = qiskit_to_tk(circuit)
-    SequencePass(pass_list).apply(tk_circuit)
-    circuit = tk_to_qiskit(tk_circuit)
-
-    return [circuit]
-
-def high_optimisation(circuit:QuantumCircuit, backend) -> list[QuantumCircuit]:
-    """Perform thourough but generic optimisation using 
-    TKET optimisation tool.
-
-    :param circuit: Circuit to be optimised.
-    :type circuit: QuantumCircuit
-    :param backend: Backend which circuit should be optimised to.
-    :type backend: BaseBackend
-    :return: Optimised circuit.
-    :rtype: list[QuantumCircuit]
-    """
-    print("  ... performing high TKET optimisation.")
-
-    pass_list = gen_pass_list(backend, quick=False, approximate=False)
-
-    # Optimise circuit using constructed pass list
-    tk_circuit = qiskit_to_tk(circuit)
-    SequencePass(pass_list).apply(tk_circuit)
-    circuit = tk_to_qiskit(tk_circuit)
-
-    return [circuit]
-
-def quick_optimisation(circuit:QuantumCircuit, backend) -> list[QuantumCircuit]:
-    """Perform basic compilation to build valid circuit for backend.
-
-    :param circuit: Circuit to be compiled.
-    :type circuit: QuantumCircuit
-    :param backend: Backend to compile to.
-    :type backend: BaseBackend
-    :return: Compiled circuit.
-    :rtype: list[QuantumCircuit]
-    """
-    print("  ... performing quick TKET optimisation.")
-
-    pass_list = gen_pass_list(backend, quick=True, approximate=False)
-
-    # Optimise circuit using constructed pass list
-    tk_circuit = qiskit_to_tk(circuit)
-    SequencePass(pass_list).apply(tk_circuit)
-    circuit = tk_to_qiskit(tk_circuit)
-
-    return [circuit]
+    return transformation_method
