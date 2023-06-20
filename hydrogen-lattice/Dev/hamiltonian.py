0999ed8c9f934048fc7168727b00185a96ffaeef
Sonny Rappaport <str36@cornell.edu>
2023-06-20 12:14:17 -0400

    feat: add h10 sheet/pyramid, json generation
    - added old h10 sheet/pyramid code to be generalized (only chains/rings
      are general for now)
    
    - json generation: run hamiltonian.py (for now) to acquire .json.
      eventually should be moved into main.py

--- hydrogen-lattice/Dev/hamiltonian.py
+++ hydrogen-lattice/Dev/hamiltonian.py
@@ -1,33 +1,126 @@
-import numpy as np 
-from qiskit_nature.converters.second_quantization import QubitConverter
-from qiskit_nature.second_q.formats.molecule_info import MoleculeInfo
-from qiskit_nature.second_q.drivers import PySCFDriver
-from qiskit_nature.mappers.second_quantization import JordanWignerMapper
-from qiskit_nature.problems.second_quantization.electronic import (
-    ElectronicStructureProblem,
-)
-from qiskit_nature.second_q.mappers import JordanWignerMapper
 import json
 
+import numpy as np
 
-def chain(n=10, r=1.0,file=True):
+from qiskit_nature.second_q.drivers import PySCFDriver
+from qiskit_nature.second_q.formats.molecule_info import MoleculeInfo
+from qiskit_nature.second_q.mappers import JordanWignerMapper
+
+
+def ring(n=10, r=1.0):
 
     xyz = np.zeros((n, 3))
     atoms = ['H'] * n
 
-    z = np.arange(-(n-1)/2, (n)/2) * r
+    angle = 2 * np.pi / n
+    rad_denom = 2 * np.sin(np.pi / n)
+    rad = r / rad_denom
+
+    x = [rad * np.cos(angle * i) for i in range(n)]
+    y = [rad * np.sin(angle * i) for i in range(n)]
+
+    xyz[:, 0] = x
+    xyz[:, 1] = y
+
+    description = "H" + str(n) + " ring, " + str(r) + " Angstroms\n"
+
+    return atoms, xyz, description
+
+
+def h10_pyramid(n=10, r=1.0):
+
+    xyz = np.zeros((n, 3))
+    atoms = ['H'] * n
+
+    hpyr = np.sqrt(2.0 / 3) * r
+    htri = np.cos(np.pi / 6.0) * r
+
+    xyz[0, 0] = 0.0
+    xyz[1, 0] = -0.5 * r
+    xyz[2, 0] = 0.5 * r
+    xyz[3, 0] = -r
+    xyz[4, 0] = 0.0
+    xyz[5, 0] = r
+    xyz[6, 0] = 0.0
+    xyz[7, 0] = -0.5 * r
+    xyz[8, 0] = 0.5 * r
+    xyz[9, 0] = 0.0
+
+    xyz[0, 1] = (4.0 / 3) * htri
+    xyz[1, 1] = (1.0 / 3) * htri
+    xyz[2, 1] = (1.0 / 3) * htri
+    xyz[3, 1] = -(2.0 / 3) * htri
+    xyz[5, 1] = -(2.0 / 3) * htri
+    xyz[6, 1] = (2.0 / 3) * htri
+    xyz[7, 1] = -(1.0 / 3) * htri
+    xyz[8, 1] = -(1.0 / 3) * htri
+    xyz[9, 1] = 0.0
+
+    xyz[0, 2] = -hpyr
+    xyz[1, 2] = -hpyr
+    xyz[2, 2] = -hpyr
+    xyz[3, 2] = -hpyr
+    xyz[4, 2] = -hpyr
+    xyz[5, 2] = -hpyr
+    xyz[6, 2] = 0.0
+    xyz[7, 2] = 0.0
+    xyz[8, 2] = 0.0
+    xyz[9, 2] = hpyr
+
+    description = "H10 pyramid, " + str(r) + " Angstroms\n"
+
+    return as_xyz(atoms, xyz, description)
+
+
+def h10_sheet(n=10, r=1.0):
+
+    xyz = np.zeros((n, 3))
+    atoms = ['H'] * n
+
+    htri = np.cos(np.pi / 6.0) * r
+
+    xyz[0, 0] = -r
+    xyz[1, 0] = 0.0
+    xyz[2, 0] = r
+    xyz[3, 0] = -(3.0 / 2) * r
+    xyz[4, 0] = -r / 2
+    xyz[5, 0] = r / 2
+    xyz[6, 0] = (3.0 / 2) * r
+    xyz[7, 0] = -r
+    xyz[8, 0] = 0.0
+    xyz[9, 0] = r
+
+    xyz[0, 1] = htri
+    xyz[1, 1] = htri
+    xyz[2, 1] = htri
+    xyz[3, 1] = 0.0
+    xyz[4, 1] = 0.0
+    xyz[5, 1] = 0.0
+    xyz[6, 1] = 0.0
+    xyz[7, 1] = -htri
+    xyz[8, 1] = -htri
+    xyz[9, 1] = -htri
+
+    description = "H10 2D sheet, " + str(r) + " Angstroms\n"
+
+    return as_xyz(atoms, xyz, description)
+
+
+def chain(n=10, r=1.0, ):
+
+    xyz = np.zeros((n, 3))
+    atoms = ['H'] * n
+
+    z = np.arange(-(n - 1) / 2, (n) / 2) * r
     assert len(z) == n
     assert sum(z) == 0.0
 
     xyz[:, 2] = z
 
     description = "H" + str(n) + " 1D chain, " + str(r) + " Angstroms\n"
-    
-    if file:
-        with open('h'+str(n)+'_'+str(r)+'_'+'chain'+'.json','w') as f:
-            print(as_xyz(atoms,xyz,description),file=f)
-    
-    return atoms, xyz
+
+    return atoms, xyz, description
+
 
 def as_xyz(atoms, xyz, description='\n'):
     # format as .XYZ
@@ -36,92 +129,75 @@ def as_xyz(atoms, xyz, description='\n'):
     pretty_xyz += description
     for i in range(n):
         pretty_xyz += "{0:s} {1:10.4f} {2:10.4f} {3:10.4f}\n".format(
-                      atoms[i], xyz[i,0], xyz[i,1], xyz[i,2]) 
+                      atoms[i], xyz[i, 0], xyz[i, 1], xyz[i, 2])
 
     return pretty_xyz
 
-#old code: uses deprecated code that can keep if desired (but code below also
 
-#def generate_qubit_hamiltonian(n,r):
-#    # Generate chain atoms and positions
-#    atoms, xyz = chain(n,r)
-#
-#    # Put atoms + posititions into a formatted list
-#    geometry =  [[atom, position] for atom, position in zip(atoms, xyz)]
-#
-#    print(geometry)
-#    # Define the molecular structure (in this case, it's a hydrogen molecule)
-#    hydrogen_molecule = Molecule(
-#            geometry=geometry, charge=0, multiplicity=1
-#            )
-#    # Prepare and run the initial Hartree-Fock calculation on molecule
-#    molecule_driver = ElectronicStructureMoleculeDriver(
-#            hydrogen_molecule, basis="sto3g", driver_type=ElectronicStructureDriverType.PYSCF
-#            )
-#    quantum_molecule = molecule_driver.run()
-#    # Get nuclear repulsion + frozen core energy (constant)
-#    nuclear_repulsion_energy = quantum_molecule.get_property("ElectronicEnergy").nuclear_repulsion_energy
-#    # Define the electronic structure problem for Quantum Processing Unit (QPU)
-#    electronic_structure_problem = ElectronicStructureProblem(molecule_driver)
-#    # Generate the second-quantized operators (fermionic hamiltonian)
-#    fermionic_hamiltonian = electronic_structure_problem.second_q_ops()["ElectronicEnergy"]
-#    # Get some useful information of number qubits and electrons, etc.
-#    grouped_property = electronic_structure_problem.grouped_property_transformed
-#    particle_number = grouped_property.get_property("ParticleNumber")
-#    num_particles = (particle_number.num_alpha, particle_number.num_beta)
-#    num_spin_orbitals = particle_number.num_spin_orbitals
-#    # Map fermionic Hamiltonian to qubit Hamiltonian. We use JW encoding.
-#    qubit_converter = QubitConverter(JordanWignerMapper())
-#    qubit_hamiltonian = qubit_converter.convert(
-#            fermionic_hamiltonian, num_particles=num_particles
-#            )
-#
-#    return qubit_hamiltonian
-
-def generate_qubit_hamiltonian(n,r, file=False):
+def generate_qubit_hamiltonian(atoms, xyz):
     # Generate chain atoms and positions
-    atoms, xyz = chain(n,r)
-    #print(atoms, xyz)
 
-    #define the molecular structure 
+    # define the molecular structure
     hydrogen_molecule = MoleculeInfo(atoms, xyz, charge=0, multiplicity=1)
 
     # Prepare and run the initial Hartree-Fock calculation on molecule
-    molecule_driver = PySCFDriver.from_molecule(hydrogen_molecule, basis= "sto3g")
-    #print("Spin:", molecule_driver.spin)
-    #print("Atom:", molecule_driver.atom)
+    molecule_driver = PySCFDriver.from_molecule(
+        hydrogen_molecule, basis="sto3g")
+    # print("Spin:", molecule_driver.spin)
+    # print("Atom:", molecule_driver.atom)
     quantum_molecule = molecule_driver.run()
 
-    # acquire fermionic hamiltonian 
+    # acquire fermionic hamiltonian
     fermionic_hamiltonian = quantum_molecule.hamiltonian.second_q_op()
 
-    #create mapper from fermionic to spin basis
+    # create mapper from fermionic to spin basis
     mapper = JordanWignerMapper()
 
-    #create qubit hamiltonian from fermionic one
+    # create qubit hamiltonian from fermionic one
     qubit_hamiltonian = mapper.map(fermionic_hamiltonian)
-    
-    if file:
 
-        op = qubit_hamiltonian.primitive.to_list()
-        n_terms = len(op)
-        coeffs = []
-        paulis = []
-
-        for i in range(n_terms):
-            coeffs.append(complex(op[i][1]).real)
-            paulis.append(op[i][0])
-
-        pauli_dict = dict(zip(paulis,coeffs))
-        pauli_json = json.dumps(pauli_dict)
-
-        with open('h'+str(n)+'_'+str(r)+'_'+'chain'+'.json','w') as f:
-            f.write(pauli_json)
-    
     return qubit_hamiltonian
 
 
 if __name__ == '__main__':
-    n = 2
-    R = 1.00
-    print(generate_qubit_hamiltonian(n,R,True))
+
+    for shape in [chain, ring]:
+        for n in [4]:  # should probably let user set n + r at some point
+            for r in [1.0]:
+
+                # get lattice info from a particular shape
+                atoms, xyz, description = shape(n, r)
+                print(xyz)
+                # log in console
+                print(as_xyz(atoms, xyz, description))
+
+                # store position info in a .json
+
+                # generate hamiltonian in spin basis
+                qubit_hamiltonian = generate_qubit_hamiltonian(atoms, xyz)
+
+                # begin putting spin basis hamiltonian into json
+                op = qubit_hamiltonian.primitive.to_list()
+
+                n_terms = len(op)
+                coeffs = []
+                paulis = []
+
+                for i in range(n_terms):
+                    coeffs.append(complex(op[i][1]).real)
+                    paulis.append(op[i][0])
+
+                pauli_dict = dict(zip(paulis, coeffs))
+
+                d = {
+                    "positions": {
+                        "x": xyz[:, 0].tolist(),
+                        "y": xyz[:, 1].tolist(),
+                        "z": xyz[:, 2].tolist(),
+                    },
+                    "hamiltonian": pauli_dict
+                }
+
+                with open('h' + str(n) + '_' + str(r) + '_' + str(shape.__name__) + '.json', 'w') as f:
+                    json.dump(d, f)
+                # end putting spin basis hamiltonian into json
