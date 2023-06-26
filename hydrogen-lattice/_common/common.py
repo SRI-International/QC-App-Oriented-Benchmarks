6aceb4c42cb82e758db2c12c557d39e1c297d721
toddytharmonkey <str36@cornell.edu>
2023-06-26 14:15:43 -0400

    - feat: added clarificaiton to debugging script

--- hydrogen-lattice/_common/common.py
+++ hydrogen-lattice/_common/common.py
@@ -168,18 +168,20 @@ def read_puccd_solution(
 if __name__ == "__main__":
     file_path = "instances/h2_chain_0.75"
 
-    print(f"File is named {file_path}")
+    print(f"File is named {file_path}. Now test printing some common.py functions.")
 
     methods_to_print = [read_paired_instance, read_jw_instance]
 
     for method in methods_to_print:
-        print(f"printing {method.__name__}:")
+        print(f"printing {method.__name__} hamiltonian information:")
 
         print(
             method(
                 file_path + ".json",
             )
         )
+
+    print("now printing solution information:")
 
     print(
         read_puccd_solution(
