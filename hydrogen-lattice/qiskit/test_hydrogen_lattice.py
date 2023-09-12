32546de0f74b316cfefde72afc8231d795b78e70
jjgoings <jjgoings@gmail.com>
2023-09-12 12:06:54 -0700

    add some more simple consistency tests

--- hydrogen-lattice/qiskit/test_hydrogen_lattice.py
+++ hydrogen-lattice/qiskit/test_hydrogen_lattice.py
@@ -1,5 +1,6 @@
 import hydrogen_lattice_benchmark
 import numpy as np
+from scipy.optimize import minimize
 
 
 def test_hydrogen_lattice_hartree_fock():
@@ -13,5 +14,43 @@ def test_hydrogen_lattice_hartree_fock():
                 thetas_array=[0.0],  # all thetas are zero
                 backend_id="statevector_simulator",
             )
-            energy, key_metrics = hydrogen_lattice_benchmark.run_objective_function(**hl_app_args)
+            energy, key_metrics = hydrogen_lattice_benchmark.run_objective_function(
+                **hl_app_args
+            )
             assert np.isclose(energy, key_metrics["hf_energy"])
+
+
+def test_h2_converges_to_exact():
+    for radius in [0.75, 1.0, 1.25]:
+        # Initial guess for the thetas_array
+        initial_guess = [0.0]
+
+        # Arguments for run_objective_function
+        hl_app_args = {
+            "num_qubits": 2,
+            "radius": radius,
+            "parameter_mode": 2,
+            "backend_id": "statevector_simulator",
+        }
+
+        # Variable to hold key_metrics
+        key_metrics_output = {}
+
+        # Objective function to minimize
+        def objective_func(x):
+            energy, key_metrics = hydrogen_lattice_benchmark.run_objective_function(
+                thetas_array=x, **hl_app_args
+            )
+            key_metrics_output.update(key_metrics)
+            return energy
+
+        # Run the optimization
+        result = minimize(objective_func, initial_guess, method="L-BFGS-B")
+
+        # Extract optimized thetas_array and minimum energy
+        optimized_thetas_array = result.x
+        minimum_energy = result.fun
+
+        # FCI = DOCI for minimal basis H2
+        assert np.isclose(minimum_energy, key_metrics_output["doci_energy"])
+        assert np.isclose(minimum_energy, key_metrics_output["fci_energy"])
