359247136f55ce2992ede0b097e423fbf2f52921
rtvuser1 <tlubinski1@gmail.com>
2024-07-28 15:29:46 -0700

    Remove reference to hamlib kernel from exact functions, by moving create_circuit back to benchmark code

--- hamlib/_common/hamiltonian_simulation_exact.py
+++ hamlib/_common/hamiltonian_simulation_exact.py
@@ -1,159 +1,33 @@
 import sys
 import os
 
-sys.path[1:1] = ["_common", "_common/qiskit"]
-sys.path[1:1] = ["../../_common", "../../_common/qiskit"]
-sys.path[1:1] = ["../qiskit"]
-
 from qiskit import QuantumCircuit 
 from qiskit.quantum_info import SparsePauliOp
 from qiskit_algorithms import TimeEvolutionProblem, SciPyRealEvolver
 from qiskit_aer import Aer
 from qiskit import transpile
 import numpy as np
-np.random.seed(0)
-import execute as ex
-import metrics as metrics
 
-from hamlib_simulation_kernel import initial_state, create_circuit
 
-def construct_TFIM_hamiltonian(n_spins: int) -> SparsePauliOp:
-    """
-    Construct the Transverse Field Ising Model (TFIM) Hamiltonian.
-
-    Args:
-        n_spins (int): Number of spins (qubits).
-
-    Returns:
-        SparsePauliOp: The Hamiltonian represented as a sparse Pauli operator.
-    """
-    pauli_strings = []
-    coefficients = []
-    g = 1  # Strength of the transverse field
-
-    # Pauli spin vector product terms
-    for i in range(n_spins):
-        x_term = 'I' * i + 'X' + 'I' * (n_spins - i - 1)
-        pauli_strings.append(x_term)
-        coefficients.append(g)
-
-    identity_string = ['I'] * n_spins
-
-    # ZZ operation on each pair of qubits in a linear chain
-    for j in range(2):
-        for i in range(j % 2, n_spins - 1, 2):
-            zz_term = identity_string.copy()
-            zz_term[i] = 'Z'
-            zz_term[(i + 1) % n_spins] = 'Z'
-            zz_term = ''.join(zz_term)
-            pauli_strings.append(zz_term)
-            coefficients.append(1.0)
-
-    return SparsePauliOp.from_list(zip(pauli_strings, coefficients))
-
-def construct_heisenberg_hamiltonian(n_spins: int, w: int, hx: list[float], hz: list[float]) -> SparsePauliOp:
-    """
-    Construct the Heisenberg Hamiltonian with disorder.
-
-    Args:
-        n_spins (int): Number of spins (qubits).
-        w (float): Strength of two-qubit interactions for heisenberg hamiltonian. 
-        hx (list[float]): Strength of internal disorder parameter for heisenberg hamiltonian. 
-        hz (list[float]): Strength of internal disorder parameter for heisenberg hamiltonian. 
-
-    Returns:
-        SparsePauliOp: The Hamiltonian represented as a sparse Pauli operator.
-    """
-
-    pauli_strings = []
-    coefficients = []
-
-    # Disorder terms
-    for i in range(n_spins):
-        x_term = 'I' * i + 'X' + 'I' * (n_spins - i - 1)
-        z_term = 'I' * i + 'Z' + 'I' * (n_spins - i - 1)
-        pauli_strings.append(x_term)
-        coefficients.append(w * hx[i])
-        pauli_strings.append(z_term)
-        coefficients.append(w * hz[i])
-
-    identity_string = ['I'] * n_spins
-
-    # Interaction terms
-    for j in range(2):
-        for i in range(j % 2, n_spins - 1, 2):
-            xx_term = identity_string.copy()
-            yy_term = identity_string.copy()
-            zz_term = identity_string.copy()
-
-            xx_term[i] = 'X'
-            xx_term[(i + 1) % n_spins] = 'X'
-
-            yy_term[i] = 'Y'
-            yy_term[(i + 1) % n_spins] = 'Y'
-
-            zz_term[i] = 'Z'
-            zz_term[(i + 1) % n_spins] = 'Z'
-
-            pauli_strings.append(''.join(xx_term))
-            coefficients.append(1.0)
-            pauli_strings.append(''.join(yy_term))
-            coefficients.append(1.0)
-            pauli_strings.append(''.join(zz_term))
-            coefficients.append(1.0)
-
-    return SparsePauliOp.from_list(zip(pauli_strings, coefficients))
-
-def construct_hamiltonian(n_spins: int, hamiltonian: str, w: float, hx : list[float], hz: list[float]) -> SparsePauliOp:
-    """
-    Construct the Hamiltonian based on the specified method.
-
-    Args:
-        n_spins (int): Number of spins (qubits).
-        hamiltonian (str): Which hamiltonian to run. "Heisenberg" by default but can also choose "TFIM". 
-        w (float): Strength of two-qubit interactions for heisenberg hamiltonian. 
-        hx (list[float]): Strength of internal disorder parameter for heisenberg hamiltonian. 
-        hz (list[float]): Strength of internal disorder parameter for heisenberg hamiltonian. 
-
-    Returns:
-        SparsePauliOp: The constructed Hamiltonian.
-    """
-
-    hamiltonian = hamiltonian.strip().lower()
-
-    if hamiltonian == "heisenberg":
-        return construct_heisenberg_hamiltonian(n_spins, w, hx, hz)
-    elif hamiltonian == "tfim":
-        return construct_TFIM_hamiltonian(n_spins)
-    else:
-        raise ValueError("Invalid Hamiltonian specification.")
-
-def HamiltonianSimulationExact(n_spins: int, init_state=None):
+def HamiltonianSimulationExact(qc_initial, n_spins: int, hamiltonian_op = None, time: float = 1.0):
     """
     Perform exact Hamiltonian simulation using classical matrix evolution.
 
     Args:
         n_spins (int): Number of spins (qubits).
-        t (float): Duration of simulation.
+        hamiltonian_op (Object): A hamiltonian operator.
+        time (float): Duration of simulation, default = 1.0
         init_state (str): The chosen initial state. By default applies the checkerboard state, but can also be set to "ghz", the GHZ state.
-        hamiltonian (str): Which hamiltonian to run. "Heisenberg" by default but can also choose "TFIM". 
-        w (float): Strength of two-qubit interactions for heisenberg hamiltonian. 
-        hx (list[float]): Strength of internal disorder parameter for heisenberg hamiltonian. 
-        hz (list[float]): Strength of internal disorder parameter for heisenberg hamiltonian. 
 
     Returns:
         dict: The distribution of the evolved state.
     """
-    _, hamiltonian_sparse, _ = create_circuit(n_spins=n_spins, init_state=init_state)
-    time_problem = TimeEvolutionProblem(hamiltonian_sparse, 1, initial_state=initial_state(n_spins, init_state))
+    time_problem = TimeEvolutionProblem(hamiltonian_op, time, initial_state=qc_initial)
     result = SciPyRealEvolver(num_timesteps=1).evolve(time_problem)
-    
-    # if verbose:
-    #   print (result)
     
     return result.evolved_state.probabilities_dict()
 
-def HamiltonianSimulation_Noiseless(qc, num_qubits, circuit_id: str ="0", num_shots=100):
+def HamiltonianSimulation_Noiseless(qc, num_qubits, circuit_id: str="0", num_shots=100):
     """
     Simulate a quantum Hamiltonian circuit for a specified number of spins using a noiseless quantum simulator.
     
