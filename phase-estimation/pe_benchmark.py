bd060525379c7af3c55db12bad7fbc19fddecda6
Neer Patel <neerpatel@macbookpro.myfiosgateway.com>
2025-06-12 13:04:22 -0400

    Modified to return circuits.

--- phase-estimation/pe_benchmark.py
+++ phase-estimation/pe_benchmark.py
@@ -116,7 +116,7 @@ def run(min_qubits=3, max_qubits=8, skip_qubits=1, max_circuits=3, num_shots=100
 		init_phase=None,
 		backend_id=None, provider_backend=None,
 		hub="ibm-q", group="open", project="main", exec_options=None,
-		context=None, api=None):
+		context=None, api=None, get_circuits=False):
 
 	# configure the QED-C Benchmark package for use with the given API
 	PhaseEstimation, kernel_draw = qedc_benchmarks_init(api)
@@ -141,6 +141,11 @@ def run(min_qubits=3, max_qubits=8, skip_qubits=1, max_circuits=3, num_shots=100
 	
 	# Initialize metrics module
 	metrics.init_metrics()
+
+	# If we want to return circuits, store them
+	if get_circuits:
+		all_qcs = []
+		all_qcs_info = []
 
 	# Define custom result handler
 	def execution_handler(qc, result, num_qubits, theta, num_shots):
@@ -171,7 +176,10 @@ def run(min_qubits=3, max_qubits=8, skip_qubits=1, max_circuits=3, num_shots=100
 		# determine number of circuits to execute for this group
 		num_circuits = min(2 ** (num_counting_qubits), max_circuits)
 		
-		print(f"************\nExecuting [{num_circuits}] circuits with num_qubits = {num_qubits}")
+		if not get_circuits:
+			print(f"************\nExecuting [{num_circuits}] circuits with num_qubits = {num_qubits}")
+		else:
+			print(f"************\nCreating [{num_circuits}] circuits with num_qubits = {num_qubits}")
 		
 		# determine range of secret strings to loop over
 		if 2**(num_counting_qubits) <= max_circuits:
@@ -195,14 +203,32 @@ def run(min_qubits=3, max_qubits=8, skip_qubits=1, max_circuits=3, num_shots=100
 			mpi.barrier()
 			ts = time.time()
 			qc = PhaseEstimation(num_qubits, theta)
-			metrics.store_metric(num_qubits, theta, 'create_time', time.time() - ts)
+			create_time = time.time() - ts
+			metrics.store_metric(num_qubits, theta, 'create_time', create_time)
+
+			# Store information if returning circuits
+			if get_circuits:
+				all_qcs.append(qc)
+				all_qcs_info.append({
+					"theta": theta,
+					"num_qubits": num_qubits,
+					"create_time": create_time
+				})
+				# Continue to skip sumbitting the circuit for execution. 
+				continue
 			
 			# submit circuit for execution on target (simulator, cloud simulator, or hardware)
 			ex.submit_circuit(qc, num_qubits, theta, num_shots)
 
 		# Wait for some active circuits to complete; report metrics when groups complete
-		ex.throttle_execution(metrics.finalize_group)
+		if not get_circuits:
+			ex.throttle_execution(metrics.finalize_group)
 
+	# Early return if we just want the circuits
+	if get_circuits:
+		print(f"************\nReturning circuits and circuit information")
+		return all_qcs, all_qcs_info
+	
 	# Wait for all active circuits to complete; report metrics when groups complete
 	ex.finalize_execution(metrics.finalize_group)
 
