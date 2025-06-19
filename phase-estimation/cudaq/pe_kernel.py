57c24f68b88f04ba54d4526ee925b228a768692c
Anishgiri01 <anish.giri@vanderbilt.edu>
2025-06-19 14:22:42 -0400

    Phase Estimation with Dynamic Circuits

--- phase-estimation/cudaq/pe_kernel.py
+++ phase-estimation/cudaq/pe_kernel.py
@@ -47,7 +47,7 @@ def iqft(register: cudaq.qview):
     
 @cudaq.kernel           
 #def pe_kernel (num_qubits: int, theta: float, do_uopt: bool):
-def pe_kernel (num_qubits: int, theta: float):
+def pe_kernel (num_qubits: int, theta: float, use_midcircuit_measurement: bool):
     M_PI = 3.1415926536
     
     init_phase = 2 * M_PI * theta
