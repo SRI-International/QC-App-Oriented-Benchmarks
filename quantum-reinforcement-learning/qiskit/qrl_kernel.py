6bd682b65c8d1b30ca29243b8991fd1deabfd5ed
Hrushikesh Patil <hrushikeshpatil890@gmail.com>
2025-07-03 15:50:27 -0400

    Qrl benchmark methods

--- quantum-reinforcement-learning/qiskit/qrl_kernel.py
+++ quantum-reinforcement-learning/qiskit/qrl_kernel.py
@@ -1,4 +1,12 @@
+'''
+Quantum Reinforcement Learning Program - Qiskit Kernel
+(C) Quantum Economic Development Consortium (QED-C) 2025.
+'''
+
 from qiskit import QuantumCircuit
+import numpy as np
+
+QC_ = None # Quantum Circuit
 
 def generate_pqc_circuit(n_qubits, n_layers, initial_state, w_params, n_measurements, index):
     qc = QuantumCircuit(n_qubits)
@@ -13,6 +21,10 @@ def generate_pqc_circuit(n_qubits, n_layers, initial_state, w_params, n_measurem
         for i in range(n_qubits-1):
             qc.cz(i, i+1)
     qc.measure_all()
+
+    global QC_
+    if QC_ == None or n_qubits <= 6:
+        if n_qubits < 9: QC_ = qc
     return qc
 
 def get_gradient_cirucits(n_qubits, n_layers, initial_state, w_params, n_measurements, index):
@@ -26,3 +38,11 @@ def get_gradient_cirucits(n_qubits, n_layers, initial_state, w_params, n_measure
     
     return grads_list
 
+############### QRL Circuit Drawer
+
+# Draw the circuits of this benchmark program
+
+def kernel_draw():
+    print("Sample Circuit:");
+    print(QC_ if QC_ != None else "  ... too large!")
+    
--- quantum-reinforcement-learning/qrl_benchmark.py
+++ quantum-reinforcement-learning/qrl_benchmark.py
@@ -1,34 +1,60 @@
-import gymnasium as gym
+import os, sys
+import time
 import numpy as np
 
+############### Configure API
+# 
+# Configure the QED-C Benchmark package for use with the given API
+def qedc_benchmarks_init(api: str = "qiskit"):
+    if api == None: api = "qiskit"
 
-def create_env():
-    env = gym.make("FrozenLake-v1")
-    return env
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    down_dir = os.path.abspath(os.path.join(current_dir, f"{api}"))
+    sys.path = [down_dir] + [p for p in sys.path if p != down_dir]
 
-def choose_action(q_values, epsilon, env):
-    if np.random.random() < epsilon:
-        return env.action_space.sample()
-    else:
-        return np.argmax(q_values)
+    up_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    common_dir = os.path.abspath(os.path.join(up_dir, "_common"))
+    sys.path = [common_dir] + [p for p in sys.path if p != common_dir]
     
-def isDone(state, env, steps, max_steps):
-    if state == env.goal_state:
-        return True
-    elif state == env.hole_state:
-        return True
-    elif steps > max_steps:
-        return True
-    else:
-        return False
+    api_dir = os.path.abspath(os.path.join(common_dir, f"{api}"))
+    sys.path = [api_dir] + [p for p in sys.path if p != api_dir]
+
+    import qcb_mpi as mpi
+    globals()["mpi"] = mpi
+    mpi.init()
+
+    import execute as ex
+    globals()["ex"] = ex
+
+    import metrics as metrics
+    globals()["metrics"] = metrics
+
+    from qrl_kernel import generate_pqc_circuits, get_gradient_cirucits, kernel_draw
     
+    return generate_pqc_circuits, get_gradient_cirucits, kernel_draw
 
 
-def train_q_values(env, q_values, num_episodes, alpha, gamma, epsilon, max_steps):
-    for episode in range(num_episodes):
-        state, info = env.reset()
-        done = False
-        while not done:
-            action = choose_action(q_values, epsilon, env)
-            next_state, reward, done, info = env.step(action)
-            q_values[state, action] = (1 - alpha) * q_values[state, action] + alpha * (reward + gamma * np.max(q_values[next_state]))
+benchmark_name = 'Quantum-Reinforcement-Learning'
+
+np.random.seed(0)
+
+verbose = False
+
+############### Result Data Analysis
+
+# Analyze and print measured results
+# Expected result is simulated
+
+def analyze_and_print_result (qc, result, num_qubits, secret_int, num_shots):
+    # size of input is one less than available qubits
+	input_size = num_qubits - 1
+	
+	# obtain counts from the result object
+	counts = result.get_counts(qc)
+	
+	# correct distribution is measuring the key 100% of the time
+	correct_dist = 
+	# use our polarization fidelity rescaling
+	fidelity = metrics.polarization_fidelity(counts, correct_dist)
+		
+	return counts, fidelity
